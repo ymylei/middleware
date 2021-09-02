3cda740d5a2726d5dd9b8fb8bc03986489dde82f
themylogin <themylogin@gmail.com>
2021-09-02 15:19:57 +0200

    Use OAuth access token for support-proxy requests

--- src/middlewared/middlewared/plugins/support.py
+++ src/middlewared/middlewared/plugins/support.py
@@ -6,7 +6,7 @@ import time
 
 from middlewared.pipe import Pipes
 from middlewared.plugins.system import DEBUG_MAX_SIZE
-from middlewared.schema import accepts, Bool, Dict, Int, List, Patch, returns, Str
+from middlewared.schema import accepts, Bool, Dict, Int, List, returns, Str
 from middlewared.service import CallError, ConfigService, job, ValidationErrors
 import middlewared.sqlalchemy as sa
 from middlewared.utils.network import INTERNET_TIMEOUT
@@ -119,14 +119,11 @@ class SupportService(ConfigService):
             ['secondary_phone', 'Secondary Contact Phone'],
         ]
 
-    # TODO: Document this please
-    @accepts(
-        Str('username'),
-        Str('password'),
-    )
-    def fetch_categories(self, username, password):
+    @accepts(Str('token'))
+    @returns(Dict(additional_attrs=True, example={'API': '11008', 'WebUI': '10004'}))
+    def fetch_categories(self, token):
         """
-        Fetch all the categories available for `username` using `password`.
+        Fetch issue categories using access token `token`.
         Returns a dict with the category name as a key and id as value.
         """
 
@@ -137,8 +134,7 @@ class SupportService(ConfigService):
             r = requests.post(
                 f'https://{ADDRESS}/{sw_name}/api/v1.0/categories',
                 data=json.dumps({
-                    'user': username,
-                    'password': password,
+                    'token': token,
                 }),
                 headers={'Content-Type': 'application/json'},
                 timeout=INTERNET_TIMEOUT,
@@ -163,8 +159,7 @@ class SupportService(ConfigService):
         Str('body', required=True, max_length=None),
         Str('category', required=True),
         Bool('attach_debug', default=False),
-        Str('username', private=True),
-        Str('password', private=True),
+        Str('token', private=True),
         Str('type', enum=['BUG', 'FEATURE']),
         Str('criticality'),
         Str('environment', max_length=None),
@@ -184,10 +179,10 @@ class SupportService(ConfigService):
         """
         Creates a new ticket for support.
         This is done using the support proxy API.
-        For FreeNAS it will be created on Redmine and for TrueNAS on SupportSuite.
+        For FreeNAS it will be created on JIRA and for TrueNAS on Salesforce.
 
-        For FreeNAS `criticality`, `environment`, `phone`, `name` and `email` attributes are not required.
-        For TrueNAS `username`, `password` and `type` attributes are not required.
+        For SCALE `criticality`, `environment`, `phone`, `name` and `email` attributes are not required.
+        For SCALE Enterprise `token` and `type` attributes are not required.
         """
 
         await self.middleware.call('network.general.will_perform_activity', 'support')
@@ -197,7 +192,7 @@ class SupportService(ConfigService):
         sw_name = 'freenas' if not await self.middleware.call('system.is_enterprise') else 'truenas'
 
         if sw_name == 'freenas':
-            required_attrs = ('type', 'username', 'password')
+            required_attrs = ('type', 'token')
         else:
             required_attrs = ('phone', 'name', 'email', 'criticality', 'environment')
             data['serial'] = (await self.middleware.call('system.dmidecode_info'))['system-serial-number']
@@ -212,8 +207,6 @@ class SupportService(ConfigService):
                 raise CallError(f'{i} is required', errno.EINVAL)
 
         data['version'] = (await self.middleware.call('system.version')).split('-', 1)[-1]
-        if 'username' in data:
-            data['user'] = data.pop('username')
         debug = data.pop('attach_debug')
 
         type_ = data.get('type')
@@ -272,10 +265,8 @@ class SupportService(ConfigService):
                 'ticket': ticket,
                 'filename': debug_name,
             }
-            if 'user' in data:
-                t['username'] = data['user']
-            if 'password' in data:
-                t['password'] = data['password']
+            if 'token' in data:
+                t['token'] = data['token']
             tjob = await self.middleware.call(
                 'support.attach_ticket', t, pipes=Pipes(input=self.middleware.pipe()),
             )
@@ -311,8 +302,7 @@ class SupportService(ConfigService):
         'attach_ticket',
         Int('ticket', required=True),
         Str('filename', required=True, max_length=None),
-        Str('username', private=True),
-        Str('password', private=True),
+        Str('token', private=True),
     ))
     @returns()
     @job(pipes=["input"])
@@ -325,8 +315,6 @@ class SupportService(ConfigService):
 
         sw_name = 'freenas' if not await self.middleware.call('system.is_enterprise') else 'truenas'
 
-        if 'username' in data:
-            data['user'] = data.pop('username')
         data['ticketnum'] = data.pop('ticket')
         filename = data.pop('filename')
 
